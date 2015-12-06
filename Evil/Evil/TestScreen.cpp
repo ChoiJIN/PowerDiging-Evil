d2f9794885fc8ac6cb766a13bfc382c2b774d38d
andrew1831 <andrew1831@naver.com>
2015-12-06 20:14:59 +0900

    사과가 쫓아오는 인공지능 구현 Object_Tracking_Character(int index)로 제어

--- Evil/Evil/TestScreen.cpp
+++ Evil/Evil/TestScreen.cpp
@@ -5,16 +5,21 @@
 TestScreen::TestScreen()
 {
 	// object initializtion
-	objs.push_back(Object(Vector3(5, -18, 0)));
+	// 0번 오브젝트는 문
+	objs.push_back(Object(Vector3(GS::option.roomSize, -18, 0)));
 	objs[0].loadObj("apple");
-	objs[0].set_passable(false);
 
-	objs.push_back(Object(Vector3(10, -18, 0)));
+	objs.push_back(Object(Vector3(5, -18, 0)));
 	objs[1].loadObj("apple");
 	objs[1].set_passable(false);
-	objs[1].setTracking(true);
-	objs[1].set_type(1);
-	objs[1].trackpos(character->getPosition());
+
+	objs.push_back(Object(Vector3(10, -18, 0)));
+	objs[2].loadObj("apple");
+	objs[2].set_passable(false);
+	objs[2].setTracking(true);
+	objs[2].set_type(1);
+	objs[2].trackpos(character->getPosition());
+
 	roomBox = Box(Vector3(0.f, 0.f, 0.f), (GS::option.roomSize*2), (GS::option.roomSize*2) + 4, (GS::option.roomSize*2));
 	
 }
@@ -27,8 +32,9 @@ TestScreen::~TestScreen()
 void TestScreen::update(double delta)
 {
 	Screen::update(delta);
+	Screen::Object_Object_Collision_Detection();
 
-	Object_Tracking_Character(1);
+	Object_Tracking_Character(2);
 
 	Screen::Character_Room_Collision_Detection();
 	Screen::Character_Objects_Collision_Detection();
